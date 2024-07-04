2f614c22a3aa4c90cbdacef49cb317188b509962
Radnac <candartozan@gmail.com>
2024-07-04 09:35:48 +0300

    icons and themes added

---
+++ icons/.icons/Bibata-Original-Rose-Pine-Dawn-Windows/uninstall.bat
@@ -0,0 +1,17 @@
+@echo off
+:: ===========================================================
+::
+:: Replace the name of cursor according to the cursor schemes.
+:: Credit: https://github.com/smit-sms
+:: More Information: https://github.com/ful1e5/apple_cursor/issues/79
+::
+:: ===========================================================
+
+REG DELETE "HKCU\Control Panel\Cursors\Schemes" /v "Bibata-Original-Rose-Pine-Dawn Cursors" /f
+
+:: ===============================================================================
+:: This enables a popup message box to indicate a user for the operation complete.
+:: ===============================================================================
+echo x=msgbox("Successfully deleted the cursor!", 0+64, "Cursor") > %tmp%	mp.vbs
+wscript %tmp%	mp.vbs
+del %tmp%	mp.vbs
